0e1fd5be0ca8d39e91fd90427913486bc725206d
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-05-10 19:17:36 -0300

    scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,7 +38,76 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    news = {
+        'url': '',
+        'title': '',
+        'timestamp': '',
+        'writer': None,
+        'shares_count': '',
+        'comments_count': '',
+        'summary': '',
+        'sources': [],
+        'categories': []
+    }
+
+    selector = Selector(text=html_content)
+    news["url"] = selector.css(
+        "head link[rel='canonical']::attr(href)"
+    ).get()
+
+    news['title'] = selector.css(
+        "main h1#js-article-title::text"
+    ).get()
+
+    news['timestamp'] = selector.css(
+        "div.tec--timestamp time::attr(datetime)"
+    ).get()
+
+    writer = selector.css(
+        ".z--font-bold *::text"
+    ).get()
+
+    if(writer is not None and writer != ''):
+        formated_writer = writer
+        if writer[0] == " " and writer[-1] == " ":
+            formated_writer = writer[1:-1]
+        news['writer'] = formated_writer
+
+    shares = selector.css(
+        "nav.tec--toolbar div.tec--toolbar__item::text"
+    ).get()
+    if(shares):
+        news['shares_count'] = int(shares.strip("Compartilharam"))
+    else:
+        news['shares_count'] = 0
+
+    comments = selector.css(
+        "#js-comments-btn::attr(data-count)"
+    ).get()
+    if(comments):
+        news['comments_count'] = int(comments.strip())
+    else:
+        news['comments_count'] = 0
+
+    news['summary'] = "".join(selector.css(
+        "div.tec--article__body > p:first-child *::text"
+    ).getall())
+
+    news['sources'] = [
+        source.strip()
+        for source
+        in selector.css(
+            "div.z--mb-16 div a::text"
+        ).getall()
+    ]
+    news['categories'] = [
+        category.strip()
+        for category
+        in selector.css(
+            "#js-categories a::text"
+        ).getall()
+    ]
+    return news
 
 
 # Requisito 5
